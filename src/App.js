aee79002376e72af363339f50334eba44dd8a26b
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2023-11-18 20:04:58 -0300

    feat: Importar componente LandingPage no arquivo App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from 'react';
+import LandingPage from './Componentes/LandingPage';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+      <LandingPage />
     </div>
   );
 }
